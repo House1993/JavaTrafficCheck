1b127b92f272436b77c0ea445088c8bd1d8916d2
House <houserabbit@163.com>
2016-01-04 10:58:25 +0800

    conveniently switch caldis

--- src/Code/Solve.java
+++ src/Code/Solve.java
@@ -141,7 +141,9 @@ public class Solve {
 			Long time = myutil.DateStrToLong(timeStr);
 			double lon = Double.parseDouble(csvRow[2]);
 			double lat = Double.parseDouble(csvRow[3]);
-			double mil = Double.parseDouble(csvRow[12].split(":")[1].split("k")[0]);
+			// double mil =
+			// Double.parseDouble(csvRow[8].split(":")[1].split("k")[0]);
+			double mil = Double.parseDouble(csvRow[8]);
 			timeStrList.add(timeStr);
 			timeList.add(time);
 			longitudeList.add(lon);
@@ -277,18 +279,28 @@ public class Solve {
 	}
 
 	private double CalDistance(double x1, double y1, double x2, double y2) {
-		// x1 = Math.toRadians(x1);
-		// y1 = Math.toRadians(y1);
-		// x2 = Math.toRadians(x2);
-		// y2 = Math.toRadians(y2);
+		return CalTrueDistance(x1, y1, x2, y2);
+		// return CalEulerDistance(x1, y1, x2, y2);
+	}
+
+	private double CalTrueDistance(double x1, double y1, double x2, double y2) {
+		x1 = Math.toRadians(x1);
+		y1 = Math.toRadians(y1);
+		x2 = Math.toRadians(x2);
+		y2 = Math.toRadians(y2);
 		double tmpx = x1 - x2;
 		double tmpy = y1 - y2;
-		// double first = Math.sin(tmpx / 2);
-		// first *= first;
-		// double second = Math.sin(tmpy / 2);
-		// second = second * second * Math.cos(x1) * Math.cos(x2);
-		// double angle = 2.0 * Math.asin(Math.sqrt(first + second));
-		// return RADIUS * angle;
+		double first = Math.sin(tmpx / 2);
+		first *= first;
+		double second = Math.sin(tmpy / 2);
+		second = second * second * Math.cos(x1) * Math.cos(x2);
+		double angle = 2.0 * Math.asin(Math.sqrt(first + second));
+		return RADIUS * angle;
+	}
+
+	private double CalEulerDistance(double x1, double y1, double x2, double y2) {
+		double tmpx = x1 - x2;
+		double tmpy = y1 - y2;
 		return Math.sqrt(tmpx * tmpx + tmpy * tmpy);
 	}
 
